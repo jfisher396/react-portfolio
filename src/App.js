c806c9390f96362b5de1e6e46171c86728ffcf50
James Fisher <jfisher396@hotmail.com>
2020-08-12 14:02:27 -0700

    imported images and linked app.css

--- src/App.js
+++ src/App.js
@@ -5,28 +5,28 @@ import Navbar from "./components/Navbar"
 import AboutMe from "./pages/AboutMe/AboutMe"
 import Portfolio from './pages/Portfolio/Portfolio';
 import Contact from './pages/Contact/Contact';
-// import Portfolio from "./pages/Portfolio"
-// import Contact from "./pages/Contact"
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/about">
-          <AboutMe />
-        </Route>
-        <Route path="/portfolio">
-          <Portfolio />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-        <Route exact path="/">
-          <AboutMe />
-        </Route>
-      </Switch>
-    </Router>
+    
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/about">
+            <AboutMe />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/">
+            <AboutMe />
+          </Route>
+        </Switch>
+      </Router>
+    
   );
 }
 
