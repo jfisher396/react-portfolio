c6dd6cbbd3fa7e7934b8f67405b97b7ab7161ab4
James Fisher <jfisher396@hotmail.com>
2020-08-12 13:18:14 -0700

    navbar component created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,23 @@ import "./Navbar.css"
 class Navbar extends Component {
     render() {
         return (
-            <h1>This is the navbar</h1>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">James Fisher</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="index.html">Home<span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="portfolio.html">Portfolio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
         )
     }
 }
